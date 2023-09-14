abbc4da63a9299b30c6ef68b0374a82eaeff266b
AndoniUTT <andoni2020lopez@gmail.com>
2023-09-14 10:46:38 -0600

    02 Variables y tipo en kotlin

---
+++ src/main/kotlin/VariablesTipos.kt
@@ -0,0 +1,25 @@
+fun main() {
+    //Variables y tipos de datos kotlin
+    // val para constantes
+    //var para variables
+
+
+    val name = "Andoni"
+    var age = 39
+
+    var heigt = 65
+    var city = "Huamantla"
+
+    println("Hola soy $name y tengo$age")
+
+    val children: Int = 0
+    val height: Double = 1.68
+
+    //String Templates
+    // $ para imprimir valor de variables
+    // ${} para procesos antes de imprimir
+    println("hola soy $name y tengo $age y soy un bellako 🥵😈")
+    println("Vivo en $city")
+
+    println("En 2030 tendré ")
+}
